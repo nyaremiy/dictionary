7ed94ff0e6239ac9a2970c93a994b9f498ab07d2
Nazar Yaremii <n.yaremiy791@gmail.com>
2022-12-28 16:04:27 +0200

    Connected tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className='app font-mono'>
+      <div className='header w-full bg-cyan-900'>
+        <div className='container max-w-full px-4 w-full pt-2 pb-2'>
+          <div className='logo text-3xl tracking-widest text-slate-100 font-medium font-mono'>
+            Dictionary
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
