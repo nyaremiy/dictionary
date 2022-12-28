498941728460c36235d1e774657ef09884a3dd88
Nazar Yaremii <n.yaremiy791@gmail.com>
2022-12-28 15:14:29 +0200

    Clear project

--- client/src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import s from './Footer.module.scss';
-
-export const Footer = () => {
-  return (
-    <div className={s.footer}>
-      <div className={`${s.wrapper} .container`}>2022 ⨰Ninja Cat⨰</div>
-    </div>
-  );
-};
